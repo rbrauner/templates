3555d7149a5ba6f7fafe3effbed80bbcbe058462
Rafał Brauner <rafalbrauner@gmail.com>
2025-02-03 21:56:52 +0100

    :construction: wip

--- symfony-app-template/vite.config.ts
+++ symfony-app-template/vite.config.ts
@@ -1,18 +1,13 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import symfonyPlugin from "vite-plugin-symfony";
-
-/* if you're using React */
-// import react from '@vitejs/plugin-react';
-
 export default defineConfig({
     plugins: [
-        /* react(), // if you're using React */
         symfonyPlugin(),
     ],
     build: {
         rollupOptions: {
             input: {
-                app: "./assets/app.js"
+                app: "./assets/app.ts"
             },
         }
     },
