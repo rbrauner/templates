cacccaf04a912a3475f5ce893bbec0da2cbbc831
Rafał Brauner <rafalbrauner@gmail.com>
2025-02-03 21:27:40 +0100

    :construction: wip

--- symfony-app-template/config/bundles.php
+++ symfony-app-template/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true],
 ];
