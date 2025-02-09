13d87323c510803561e470b1ea92278927549439
Rafał Brauner <rafalbrauner@gmail.com>
2025-02-09 20:08:04 +0100

    :bug: Fixes (#8)
    * :bug: fixes
    
    * :construction: readme

--- symfony-app-template/tests/Other/SystemCheckTest.php
+++ symfony-app-template/tests/Other/SystemCheckTest.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  * Copyright (C) Rafał Brauner
  */
 
-namespace Tests\Other;
+namespace App\Tests\Other;
 
 use PHPUnit\Framework\Attributes\CoversNothing;
 use PHPUnit\Framework\TestCase;
--- symfony-console-app-template/bin/deploy.sh
+++ symfony-console-app-template/bin/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker compose up -d
-docker exec -it template-php bash -c "APP_ENV=prod composer install --no-dev --optimize-autoloader"
+docker exec -it template-php bash -c "APP_ENV=prod composer.phar install --no-ansi --no-dev --no-interaction --no-plugins --no-progress --no-scripts --optimize-autoloader"
 
 rsync -zariv --delete --mkpath -e 'ssh -p 22' bin/template user@host:/apps/template/bin/template
 rsync -zariv --delete --mkpath -e 'ssh -p 22' docker/prod/ user@host:/apps/template/docker/prod
