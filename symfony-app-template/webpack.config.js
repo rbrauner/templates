c1d2fa209768dcd4265a00b281b27a63b03b1527
Rafał Brauner <rafalbrauner@gmail.com>
2025-02-10 22:37:55 +0100

    :construction: wip

---
+++ symfony-app-template/webpack.config.js
@@ -0,0 +1,21 @@
+const Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/app.ts')
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .enableTypeScriptLoader()
+    .enableIntegrityHashes(Encore.isProduction())
+;
+
+module.exports = Encore.getWebpackConfig();
